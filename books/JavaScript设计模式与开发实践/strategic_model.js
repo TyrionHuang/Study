b01f2291eac3ecc29c5a743c4a8ed13d08a26cba
Tyrion <huangyangjian@joyintoy.cn>
2022-02-11 10:56:51 +0800

    some update

---
+++ books/JavaScript设计模式与开发实践/strategic_model.js
@@ -0,0 +1,17 @@
+const S = (salary) => {
+    return salary * 4;
+};
+
+const A = (salary) => {
+    return salary * 3;
+};
+
+const B = (salary) => {
+    return salary * 2;
+};
+
+const calculateBonus = (func, salary) => {
+    return func(salary);
+};
+
+calculateBonus(S, 10000);
